a5771f2a2cd777d951904567574b3bb24a669b0b
Gabriel Recinos <gabrielhh26@gmail.com>
2015-03-25 15:20:41 -0600

    salvando prioridades

--- app/controllers/priorities_controller.rb
+++ app/controllers/priorities_controller.rb
@@ -1,6 +1,24 @@
 class PrioritiesController < ApplicationController
   def index
 
-    
+    @priorities = Priority.order(:title)
+
   end
+
+  def create
+
+  end
+
+  def new
+    @priority = Priority.new
+  end
+
+  def update
+
+  end
+
+  def delete
+
+  end
+
 end
--- app/models/priority.rb
+++ app/models/priority.rb
@@ -1,6 +1,6 @@
 class Priority < ActiveRecord::Base
   has_many :dimentions
   validates_presence_of :title, :weight
-  validates_numericality_of :weight, greater_than_or_equal_to: 0
+  validates_numericality_of :weight, greater_than_or_equal_to: 0, less_than_or_equal_to: 100
 
 end
